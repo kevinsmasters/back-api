83a2b6027ed8288476d99eb53a3affe5aea2e065
kevinsmasters <gmail@kevinmasters.com>
2022-02-19 20:06:58 -0500

    change port to const

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const app = express();
+const port = process.env.PORT || 3000
 
 app.get('/api', (req, res) => {
     res.status(200).json({api: 'version 1'})
 });
 
-app.listen(3000, () => console.log('server started'));
+app.listen(port, () => console.log('server started on port:', port));
