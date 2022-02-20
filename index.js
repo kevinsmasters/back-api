88b78547bfd089aa18bcf7461b4b1c1398b35d01
kevinsmasters <gmail@kevinmasters.com>
2022-02-19 21:01:23 -0500

    adds cors support

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3000
+const cors = require('cors');
+const port = process.env.PORT || 3000;
+
+app.use(cors())
 
 app.get('/api', (req, res) => {
     res.status(200).json({api: 'version 1'})
